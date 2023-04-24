e937461883012dbcbae9ba80709a4af931a183c6
Christian Dahdah <christiandahdah@gmail.com>
2023-04-24 16:21:03 +0200

    fix: resolved IERC20 import in ISafeguardPool

--- pkg/interfaces/contracts/pool-safeguard/ISafeguardPool.sol
+++ pkg/interfaces/contracts/pool-safeguard/ISafeguardPool.sol
@@ -15,7 +15,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import "@balancer-labs/v2-interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
+import "../solidity-utils/openzeppelin/IERC20.sol";
 
 interface ISafeguardPool {
 
