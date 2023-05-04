27d8248b436ca7fe906708e2a7987be1a97ca51d
Christian Dahdah <christiandahdah@gmail.com>
2023-05-04 17:26:26 +0200

    helpers: fixed return value of toBNish

--- pvt/helpers/src/models/pools/safeguard/helpers.ts
+++ pvt/helpers/src/models/pools/safeguard/helpers.ts
@@ -1,6 +1,7 @@
 import { Decimal } from 'decimal.js';
 import { BigNumberish, decimal } from '../../../numbers';
+import { bn } from '../../../numbers';
 
-export const toBNish = (x: BigNumberish | Decimal, decimals: number): Decimal => decimal(x).mul(decimal(10).pow(decimals));
+export const toBNish = (x: BigNumberish | Decimal, decimals: number): BigNumberish => bn(decimal(x).mul(decimal(10).pow(decimals)));
 
 export const fromBNish = (x: BigNumberish | Decimal, decimals: number): Decimal => decimal(x).div(decimal(10).pow(decimals));
